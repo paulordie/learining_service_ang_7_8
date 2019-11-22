00d0da46023fd180c0c4c71012436c7d27a0e827
Paulo <paulo.correa@indt.org.br>
2019-11-22 12:56:32 -0400

    setup service structure basic

--- src/app/module1/module1.module.ts
+++ src/app/module1/module1.module.ts
@@ -3,8 +3,6 @@ import { CommonModule } from '@angular/common';
 import { Component1Component } from './component1/component1.component';
 import { Component2Component } from './component2/component2.component';
 
-
-
 @NgModule({
   declarations: [Component1Component, Component2Component],
   exports: [Component1Component, Component2Component],
