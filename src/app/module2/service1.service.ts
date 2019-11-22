21d0d31e1a7d8f40c2665d2ab89f2105bb8e44d5
Paulo <paulo.correa@indt.org.br>
2019-11-22 14:30:36 -0400

    service1 to module1 and module2

--- src/app/module2/service1.service.ts
+++ src/app/module2/service1.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
+import { Module2Module } from './module2.module';
+import { Module1Module } from '../module1/module1.module';
+
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: Module1Module
 })
 export class Service1Service {
-
-  constructor() { }
+  public num = 0;
+  constructor() {
+    this.num = Math.round(Math.random()*1000);
+   }
 }
